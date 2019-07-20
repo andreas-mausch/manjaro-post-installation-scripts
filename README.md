9faf8477e595979cb5380470abdfc9d083619674
neonew <neonew@gmail.com>
2019-07-20 17:24:01 +0200

    Find all scripts in subdirectories and put them in a map

---
+++ README.md
@@ -0,0 +1,5 @@
+This is a collection of my personal post-installation scripts after a clean Manjaro installation.
+
+It contains all my favorite apps, settings, fonts etc.
+
+Feel free to use and customize them.
---
+++ post-installation.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+shopt -s globstar
+
+declare -A scripts
+
+scriptFiles=(./scripts/**/script.sh)
+
+for scriptFile in "${scriptFiles[@]}"
+do
+  withoutPath=${scriptFile#"./scripts/"}
+  scriptName=${withoutPath%"/script.sh"}
+  
+  scripts[${scriptName}]=${scriptFile}
+done
+
+for KEY in "${!scripts[@]}"; do
+  # Print the KEY value
+  echo "Key: $KEY"
+  # Print the VALUE attached to that KEY
+  echo "Value: ${scripts[$KEY]}"
+done
