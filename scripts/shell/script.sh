d19664ea480967f8df1d623d76dd523385182bcb
neonew <neonew@gmail.com>
2019-07-21 14:37:06 +0200

    fish shell: install oh-my-fish

--- scripts/shell/script.sh
+++ scripts/shell/script.sh
@@ -7,3 +7,9 @@ sudo cp ./chsh /etc/pam.d/
 chsh -s /usr/bin/fish
 sudo cp ./chsh.backup /etc/pam.d/chsh
 rm ./chsh.backup
+
+git clone https://github.com/oh-my-fish/oh-my-fish
+cd ./oh-my-fish
+./bin/install --offline --noninteractive
+cd ..
+rm -rf ./oh-my-fish
