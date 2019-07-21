d38bd46d94719de058a7049b8923d7ceb4683a8a
neonew <neonew@gmail.com>
2019-07-21 13:47:19 +0200

    yay: don't compress custom built packages

--- scripts/yay/script.sh
+++ scripts/yay/script.sh
@@ -7,3 +7,5 @@ cd ./yay-build
 git clone --quiet https://aur.archlinux.org/yay.git
 cd yay
 makepkg --syncdeps --install --noconfirm
+
+sudo sed -i "s,PKGEXT='.pkg.tar.xz',PKGEXT='.pkg.tar',g" /etc/makepkg.conf
