f0e15237162e1a1556c607f21c02ff12296a88e8
neonew <neonew@gmail.com>
2019-07-22 23:43:23 +0200

    Albert: Fix broken autostart symlink

--- albert/script.sh
+++ albert/script.sh
@@ -4,4 +4,4 @@ sudo pacman -S --noconfirm albert
 mkdir -p ~/.config/albert/org.albert.frontend.qmlboxmodel/
 cp ./albert.conf ~/.config/albert/
 cp ./style_properties.ini ~/.config/albert/org.albert.frontend.qmlboxmodel/
-ln -s /usr/share/applications/albert.desktop /home/neonew/.config/autostart/albert.desktop
+ln -s /usr/share/applications/albert.desktop ~/.config/autostart/albert.desktop
