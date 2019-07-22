e3f56f335d6a77bb70cc768a89b6a562374fd3f3
neonew <neonew@gmail.com>
2019-07-22 23:53:54 +0200

    Albert: Needs muparser for the calculator

--- albert/script.sh
+++ albert/script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo pacman -S --noconfirm albert
+sudo pacman -S --noconfirm albert muparser
 mkdir -p ~/.config/albert/org.albert.frontend.qmlboxmodel/
 cp ./albert.conf ~/.config/albert/
 cp ./style_properties.ini ~/.config/albert/org.albert.frontend.qmlboxmodel/
