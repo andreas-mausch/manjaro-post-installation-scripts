0cabfb0bb2ded8233b1cd5988a3bc982b4613251
neonew <neonew@gmail.com>
2019-07-22 22:07:56 +0200

    Favorite tools

---
+++ tools/script.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+sudo pacman -S --noconfirm \
+  audacity \
+  avidemux-qt \
+  code \
+  gthumb \
+  handbrake \
+  httpie \
+  meld \
+  wireshark-qt \
+  youtube-dl
+yay -S --noconfirm \
+  fsearch-git \
+  s-tui
