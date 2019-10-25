723e44aa035568b4973f24cc26d8b448fb745a35
neonew <neonew@gmail.com>
2019-10-25 10:39:05 +0200

    Use caps-lock for terminal

--- albert/script.sh
+++ albert/script.sh
@@ -4,4 +4,8 @@ sudo pacman -S --noconfirm albert muparser
 mkdir -p ~/.config/albert/org.albert.frontend.qmlboxmodel/
 cp ./albert.conf ~/.config/albert/
 cp ./style_properties.ini ~/.config/albert/org.albert.frontend.qmlboxmodel/
-ln -s /usr/share/applications/albert.desktop ~/.config/autostart/albert.desktop
+ln -s /usr/share/applications/albert.desktop ~/.config/autostart/
+
+sudo setkeycodes e05c 0xfa
+echo "keycode 0x42 = 0xfa" >> ~/.Xmodmap
+cp ./disable-capslock.desktop ~/.config/autostart/
