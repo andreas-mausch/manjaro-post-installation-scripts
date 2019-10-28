b9320100df9552867ad2aa24d87c28584277a9ca
neonew <neonew@gmail.com>
2019-10-28 17:43:39 +0100

    alacritty: create config directory

--- terminal/script.sh
+++ terminal/script.sh
@@ -3,6 +3,7 @@
 xfconf-query -c xsettings -p /Gtk/MonospaceFontName -s "Source Code Pro 10"
 
 sudo pacman -S --noconfirm alacritty
+mkdir -p ~/.config/alacritty
 cp ./alacritty.yml ~/.config/alacritty
 
 yay -S --noconfirm tdrop-git
