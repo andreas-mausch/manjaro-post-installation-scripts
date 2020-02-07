f6b31f059fdab3a8863be79a6c131b7180612683
neonew <neonew@gmail.com>
2020-02-07 12:30:33 +0100

    .albertignore

--- albert/script.sh
+++ albert/script.sh
@@ -3,7 +3,7 @@
 sudo pacman -S --noconfirm albert muparser
 mkdir -p ~/.config/albert/org.albert.frontend.qmlboxmodel/
 cp ./albert.conf ~/.config/albert/
-cp ./style_properties.ini ~/.config/albert/org.albert.frontend.qmlboxmodel/
+cp ./.albertignore ~/
 ln -s /usr/share/applications/albert.desktop ~/.config/autostart/
 
 echo "keycode 0x42 = 0xfa" >> ~/.Xmodmap
