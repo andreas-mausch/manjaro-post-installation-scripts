82cfb4b711dcbfc17af7df0fb5e8f98e353fe7f4
neonew <neonew@gmail.com>
2020-02-13 21:34:23 +0100

    autojump is now in AUR

--- shell/script.sh
+++ shell/script.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-sudo pacman -S --noconfirm fish autojump
+sudo pacman -S --noconfirm fish
 
 cp /etc/pam.d/chsh ./chsh.backup
 sudo cp ./chsh /etc/pam.d/
 chsh -s /usr/bin/fish
 sudo cp ./chsh.backup /etc/pam.d/chsh
 rm ./chsh.backup
+
+yay -S --noconfirm autojump-git
 
 git clone https://github.com/oh-my-fish/oh-my-fish
 cd ./oh-my-fish
