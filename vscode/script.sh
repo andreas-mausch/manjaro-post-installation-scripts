f1a5f94a6e2978663592a028e40452497f1a29e6
neonew <neonew@gmail.com>
2020-02-13 17:17:55 +0100

    vscode extensions

--- vscode/script.sh
+++ vscode/script.sh
@@ -4,8 +4,10 @@ sudo pacman -S --noconfirm code
 
 code --install-extension EditorConfig.EditorConfig
 code --install-extension dbaeumer.vscode-eslint
+code --install-extension drrouman.git-coauthors
 code --install-extension eamodio.gitlens
 code --install-extension octref.vetur
+code --install-extension stylelint.vscode-stylelint
 
 mkdir -p ~/.config/Code\ -\ OSS/User/
 cp ./settings.json ~/.config/Code\ -\ OSS/User/
