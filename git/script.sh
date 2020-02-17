054880490a4d7a8f6774b78b2af83c93c2d658b3
neonew <neonew@gmail.com>
2020-02-17 10:55:15 +0100

    git: Add Tig: text-mode interface for Git
    https://jonas.github.io/tig/

--- git/script.sh
+++ git/script.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 sudo pacman -S --noconfirm tk aspell aspell-en aspell-de
+sudo pacman -S --noconfirm tig
 
 git config --global alias.lg "log --abbrev-commit --decorate --date=format:'%Y-%m-%d %H:%M:%S' --format=format:'%C(blue)%h%C(reset) %C(dim white)%ad%C(reset) %C(green)%<(8,trunc)%an%C(reset)%C(yellow)%d%C(reset) %C(white)%s%C(reset)'"
 git config --global alias.tidy "! git branch --merged origin/master | grep -v \* | xargs --no-run-if-empty git branch -D && git remote update origin --prune"
