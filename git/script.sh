ac624c003f652805d95717b70e5e1b8f3f9e3446
neonew <neonew@gmail.com>
2020-02-17 10:37:45 +0100

    git: Add missing packages for git-gui and gitk

--- git/script.sh
+++ git/script.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+sudo pacman -S --noconfirm tk aspell aspell-en aspell-de
+
 git config --global alias.lg "log --abbrev-commit --decorate --date=format:'%Y-%m-%d %H:%M:%S' --format=format:'%C(blue)%h%C(reset) %C(dim white)%ad%C(reset) %C(green)%<(8,trunc)%an%C(reset)%C(yellow)%d%C(reset) %C(white)%s%C(reset)'"
 git config --global alias.tidy "! git branch --merged origin/master | grep -v \* | xargs --no-run-if-empty git branch -D && git remote update origin --prune"
 git config --global alias.first "hash-object -t tree /dev/null"
