96b11232dad12d804b566883f2c3a13bdbbab3c3
neonew <neonew@gmail.com>
2021-07-06 16:53:39 +0200

    albert is now in aur

--- albert/script.sh
+++ albert/script.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-sudo pacman -S --noconfirm albert muparser
+sudo pacman -S --noconfirm muparser
+yay -S --noconfirm albert
+
 mkdir -p ~/.config/albert/org.albert.frontend.qmlboxmodel/
 cp ./albert.conf ~/.config/albert/
 cp ./.albertignore ~/
