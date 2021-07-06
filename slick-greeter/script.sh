06fcdca8842f7832820eae3fe400a46c0852c567
neonew <neonew@gmail.com>
2021-07-06 16:53:57 +0200

    Instructions for slick-greeter

--- slick-greeter/script.sh
+++ slick-greeter/script.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
 sudo pacman -S --noconfirm lightdm-slick-greeter lightdm-settings
+
+# Note: You manually need to edit /etc/lightdm/lightdm.conf and set
+# greeter-session=lightdm-slick-greeter
+
+# You might also want to edit the background image in
+# /etc/lightdm/slick-greeter.conf
