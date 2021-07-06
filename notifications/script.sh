b39b89b275733f62e2ab7dbdb4391cc11e758d4b
neonew <neonew@gmail.com>
2021-07-06 16:58:47 +0200

    More notifications

--- notifications/script.sh
+++ notifications/script.sh
@@ -4,3 +4,6 @@ gsettings list-keys org.gnome.nm-applet
 gsettings set org.gnome.nm-applet disable-disconnected-notifications "true"
 gsettings set org.gnome.nm-applet disable-connected-notifications "true"
 gsettings set org.gnome.nm-applet suppress-wireless-networks-available "true"
+
+# Disable "Einrasten der Tasten ist aktiviert"
+xfconf-query -c xfce4-notifyd -p /applications/muted_applications -n -t string -s xfce4-settings-helper -a
