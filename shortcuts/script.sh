27a64a42f07d91b8440dd210cc670efc6323a2a2
neonew <neonew@gmail.com>
2021-07-07 21:49:18 +0200

    Use xmodmap for caps lock
    xmodmap seems to conflict with setxkbmap
    
    In my case it worked, but only after a few seconds after login.
    With this new method, the mapping works immediately.
    
    https://unix.stackexchange.com/questions/482400/setxkbmap-conflicting-with-xmodmap

--- shortcuts/script.sh
+++ shortcuts/script.sh
@@ -2,6 +2,5 @@
 
 cp ./xfce4-keyboard-shortcuts.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/
 
-# echo "keycode 0x42 = 0xfa" >> ~/.Xmodmap # Map capslock to some unused key (0xfa = ú)
 echo "keycode 0x31 = 0xfb" >> ~/.Xmodmap # Map dead dircumflex to some unused key (0xfb = û)
-cp ./disable-capslock.desktop ~/.config/autostart/
+echo "keycode 0x42 = NoSymbol" >> ~/.Xmodmap # Map capslock to nothing
