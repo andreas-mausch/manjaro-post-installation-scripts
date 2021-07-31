3e03345e42a2078b9ed9c5e8100c38ea9dfe05f5
neonew <neonew@gmail.com>
2021-07-31 03:58:46 +0200

    Update git aliases

--- git/script.sh
+++ git/script.sh
@@ -4,8 +4,12 @@ sudo pacman -S --noconfirm tk aspell aspell-en aspell-de
 sudo pacman -S --noconfirm tig
 
 git config --global alias.lg "log --abbrev-commit --decorate --date=format:'%Y-%m-%d %H:%M:%S' --format=format:'%C(blue)%h%C(reset) %C(dim white)%ad%C(reset) %C(green)%<(8,trunc)%an%C(reset)%C(yellow)%d%C(reset) %C(white)%s%C(reset)'"
-git config --global alias.tidy "! git branch --merged origin/master | grep -v \* | xargs --no-run-if-empty git branch -D && git remote update origin --prune"
+git config --global alias.tidy.branches "! git branch --merged origin/master | grep -v \* | xargs --no-run-if-empty git branch -D && git remote update origin --prune"
+git config --global alias.tidy.tags "! git tag -l | xargs git tag -d && git fetch --tags"
+git config --global alias.tidy "! git tidy.branches && git tidy.tags"
 git config --global alias.first "hash-object -t tree /dev/null"
+git config --global alias.tags "lg --tags --no-walk"
+git config --global alias.branches "! git for-each-ref refs/heads refs/remotes/origin --format='%(refname:short)' | xargs -i sh -c 'ahead=\$(git rev-list --count origin/master..{}); behind=\$(git rev-list --count {}..origin/master); printf \"%4s %4s {}\n\" \"+\$ahead\" \"-\$behind\"'"
 
 git config --global gui.fontui "-family \"Droid Sans\" -size 12 -weight normal -slant roman -underline 0 -overstrike 0"
 git config --global gui.fontdiff "-family \"Droid Sans Mono\" -size 12 -weight normal -slant roman -underline 0 -overstrike 0"
