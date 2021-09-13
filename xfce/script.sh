ec32f91ec59b239a70d3510a3a994cfa0be485b3
neonew <neonew@gmail.com>
2021-09-13 23:47:53 +0200

    xfce4-power-manager settings

--- xfce/script.sh
+++ xfce/script.sh
@@ -4,6 +4,12 @@ rm -f ~/.config/autostart/manjaro-hello.desktop
 
 xfconf-query -c xfwm4 -p /general/workspace_count -s 1
 
+# Power settings
+
+xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/brightness-step-count -s 12
+xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/brightness-exponential -s "true"
+xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/critical-power-level -s 5
+
 # Theme
 
 xfconf-query -c xsettings -p /Net/ThemeName -s "Matcha-dark-azul"
