9643630958a870cd597d39e35753d5de70f3b3f9
neonew <neonew@gmail.com>
2022-04-21 16:48:26 +0200

    Audacious with Winamp skin

---
+++ audacious/script.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+sudo pacman -S --noconfirm audacious
+
+mkdir -p ~/.local/share/audacious/Skins/
+cp ./135799-winamp_classic.wsz ~/.local/share/audacious/Skins/
+
+mkdir -p ~/.config/audacious/
+cp ./config ~/.config/audacious/
+echo "skin=`realpath ~/.local/share/audacious/Skins/135799-winamp_classic.wsz`" >> ~/.config/audacious/config
