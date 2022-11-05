f7a7a3e21b077509c4e75571825bf7779265a776
neonew <neonew@gmail.com>
2022-11-05 22:48:34 +0100

    VirtualBox: Instructions for guest machines

--- virtualbox/script.sh
+++ virtualbox/script.sh
@@ -2,3 +2,7 @@
 
 sudo pacman -S --noconfirm virtualbox virtualbox-host-dkms
 yay -S --noconfirm virtualbox-ext-oracle
+
+# On Arch/Manjaro guest machines, run this:
+# sudo pacman -S virtualbox-guest-utils
+# https://wiki.archlinux.org/title/VirtualBox/Install_Arch_Linux_as_a_guest
