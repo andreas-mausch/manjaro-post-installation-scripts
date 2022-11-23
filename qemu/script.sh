2ea4a8842e36b906c625757e1c347342ea0f0340
neonew <neonew@gmail.com>
2022-11-23 02:37:41 +0100

    Replace VirtualBox by qemu/kvm and virt-manager

---
+++ qemu/script.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+sudo pacman -S --noconfirm libvirt virt-manager virt-viewer
+
+sudo systemctl enable libvirtd.service
+sudo systemctl start libvirtd.service
+
+# You might want to edit /etc/libvirt/libvirtd.conf
+# to use your own user.
+# unix_sock_group = "libvirt"
+# unix_sock_rw_perms = "0770"
+# sudo usermod -a -G libvirt $(whoami)
+# sudo systemctl restart libvirtd.service
--- virtualbox/script.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-sudo pacman -S --noconfirm virtualbox virtualbox-host-dkms virtualbox-guest-iso
-yay -S --noconfirm virtualbox-ext-oracle
-
-# On Arch/Manjaro guest machines, run this:
-# sudo pacman -S virtualbox-guest-utils
-# https://wiki.archlinux.org/title/VirtualBox/Install_Arch_Linux_as_a_guest
