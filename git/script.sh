2f0be0fc358c7fa413b8a88a7115b833d32a9198
neonew <neonew@gmail.com>
2023-01-24 20:05:44 +0100

    git default

--- git/script.sh
+++ git/script.sh
@@ -11,6 +11,7 @@ git config --global alias.tidy "! git tidy.branches && git tidy.tags"
 git config --global alias.first "hash-object -t tree /dev/null"
 git config --global alias.tags "lg --tags --no-walk"
 git config --global alias.branches "! git for-each-ref refs/heads refs/remotes/origin --format='%(refname:short)' | xargs -i sh -c 'ahead=\$(git rev-list --count origin/master..{}); behind=\$(git rev-list --count {}..origin/master); printf \"%4s %4s {}\n\" \"+\$ahead\" \"-\$behind\"'"
+git config --global alias.default "! git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@'"
 
 git config --global gui.fontui "-family \"Droid Sans\" -size 12 -weight normal -slant roman -underline 0 -overstrike 0"
 git config --global gui.fontdiff "-family \"Droid Sans Mono\" -size 12 -weight normal -slant roman -underline 0 -overstrike 0"
