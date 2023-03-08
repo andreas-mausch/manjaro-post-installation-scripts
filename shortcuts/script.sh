af10171df38f607f929f94598ee1a813116df6c5
neonew <neonew@gmail.com>
2023-03-08 01:33:47 +0100

    Improve caps lock

--- shortcuts/script.sh
+++ shortcuts/script.sh
@@ -1,3 +1,8 @@
 cp ./xfce4-keyboard-shortcuts.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/
 
-echo "keycode 0x42 = 0xfb" >> ~/.Xmodmap # Map capslock to some unused key (0xfb = û)
+# Disable CapsLock
+echo "clear Lock" >> ~/.Xmodmap
+
+# Map capslock to some unused key (0xfb = û)
+# but still retain functionality it if you press Shift+CapsLock
+echo "keycode 0x42 = 0xfb Caps_Lock" >> ~/.Xmodmap
