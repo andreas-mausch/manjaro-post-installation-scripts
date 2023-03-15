e26ed285e144adecb0413b0b7f7bfb4f047b4514
neonew <neonew@gmail.com>
2023-03-15 01:19:11 +0100

    Use a different background image for the login screen

--- slick-greeter/script.sh
+++ slick-greeter/script.sh
@@ -3,5 +3,8 @@ sudo pacman -S --needed --noconfirm lightdm-slick-greeter lightdm-settings
 # Note: You manually need to edit /etc/lightdm/lightdm.conf and set
 # greeter-session=lightdm-slick-greeter
 
-# You might also want to edit the background image in
-# /etc/lightdm/slick-greeter.conf
+# Login background for slick-greeter
+# Taken from here: https://github.com/ooxxvv/basil-tw
+sudo cp ./login-background.png /usr/share/backgrounds/
+sudo chown root:root /usr/share/backgrounds/login-background.png
+sudo sed -i "s,^background=.*\$,background=/usr/share/backgrounds/login-background.png,g" /etc/lightdm/slick-greeter.conf
