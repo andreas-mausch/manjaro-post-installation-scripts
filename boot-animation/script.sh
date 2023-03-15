cb2eccc6e6c067404a40ecc7a47171198741df0b
neonew <neonew@gmail.com>
2023-03-15 01:39:59 +0100

    Custom boot animation via plymouth

---
+++ boot-animation/script.sh
@@ -0,0 +1,9 @@
+# Source of theme: https://github.com/adi1090x/plymouth-themes
+sudo mkdir -p /usr/share/plymouth/themes/motion/
+unzip -q ./motion.zip -d /usr/share/plymouth/themes/motion/
+sudo plymouth-set-default-theme -R motion
+
+# I had to change a line in
+# sudo nano /etc/mkinitcpio.conf
+# MODULES="i915"
+# See here: https://wiki.archlinux.org/title/plymouth
