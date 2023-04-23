5694e968cd05c6b3d9932343e4fa090a3c30d848
neonew <neonew@gmail.com>
2023-04-23 18:09:10 +0200

    nvim: Only show normal mode keymaps on ?

--- neovim/user/mappings.lua
+++ neovim/user/mappings.lua
@@ -1,10 +1,11 @@
 local utils = require "astronvim.utils"
+local telescope = require "telescope.builtin"
 
 return {
   n = {
     ["<leader>tt"] = { function() utils.toggle_term_cmd "zenith" end, desc = "ToggleTerm zenith" },
     ["<leader>tu"] = { function() utils.toggle_term_cmd "ncdu" end, desc = "ToggleTerm ncdu" },
-    ["?"] = { function() require("telescope.builtin").keymaps() end, desc = "Find keymaps" }
+    ["?"] = { function() telescope.keymaps({ modes = { "n" } }) end, desc = "Find keymaps" }
   },
   t = {
   }
