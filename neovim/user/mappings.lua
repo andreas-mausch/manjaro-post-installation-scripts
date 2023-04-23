886e487ac226efd9445aaefad5f4100ea9fa0035
neonew <neonew@gmail.com>
2023-04-23 17:57:31 +0200

    nvim: Show keymaps on ?

--- neovim/user/mappings.lua
+++ neovim/user/mappings.lua
@@ -3,8 +3,9 @@ local utils = require "astronvim.utils"
 return {
   n = {
     ["<leader>tt"] = { function() utils.toggle_term_cmd "zenith" end, desc = "ToggleTerm zenith" },
-    ["<leader>tu"] = { function() utils.toggle_term_cmd "ncdu" end, desc = "ToggleTerm ncdu" }
+    ["<leader>tu"] = { function() utils.toggle_term_cmd "ncdu" end, desc = "ToggleTerm ncdu" },
+    ["?"] = { function() require("telescope.builtin").keymaps() end, desc = "Find keymaps" }
   },
   t = {
-  },
+  }
 }
