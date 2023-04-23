35c3de4df32e5e37c772e2152511afd31936bd61
neonew <neonew@gmail.com>
2023-04-23 10:16:38 +0200

    neovim: use ncdu instead of gdu

--- neovim/user/mappings.lua
+++ neovim/user/mappings.lua
@@ -2,7 +2,8 @@ local utils = require "astronvim.utils"
 
 return {
   n = {
-    ["<leader>tt"] = { function() utils.toggle_term_cmd "zenith" end, desc = "ToggleTerm zenith" }
+    ["<leader>tt"] = { function() utils.toggle_term_cmd "zenith" end, desc = "ToggleTerm zenith" },
+    ["<leader>tu"] = { function() utils.toggle_term_cmd "ncdu" end, desc = "ToggleTerm ncdu" }
   },
   t = {
   },
