f5a9ba67bc15a581c708b3bcfd127cc3996871e6
neonew <neonew@gmail.com>
2023-04-24 18:16:55 +0200

    nvim: Temporary fix for crashing jpegs

--- neovim/user/options.lua
+++ neovim/user/options.lua
@@ -1,5 +1,9 @@
 return {
   opt = {
+    -- Bug in gitsigns. Remove the line when it is fixed.
+    -- nvim crashes when opening a jpeg file.
+    -- https://github.com/lewis6991/gitsigns.nvim/issues/758
+    diffopt = vim.opt.diffopt - "linematch:60",
     relativenumber = false
   },
   g = {
