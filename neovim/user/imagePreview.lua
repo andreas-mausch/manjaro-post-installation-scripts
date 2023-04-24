25b0afb1602be93a080ad9a81ffaea84d25142dd
neonew <neonew@gmail.com>
2023-04-24 20:26:50 +0200

    nvim: Use buffer id and filename from event, show errors to the user

--- neovim/user/imagePreview.lua
+++ neovim/user/imagePreview.lua
@@ -3,10 +3,12 @@ vim.api.nvim_create_autocmd("BufEnter", {
   desc = "Image Preview with timg",
   pattern = "*.jpg,*.jpeg,*.png",
   callback = function(event)
-    -- require("notify")(string.format('event fired: %s', vim.inspect(event)))
-    local bufferId = 0
-    local content = { "Loading.." }
-    vim.api.nvim_buf_set_lines(bufferId, 0, -1, false, content)
+    local bufferId = event.buf
+    if not vim.api.nvim_buf_get_option(bufferId, "modifiable") then
+      return
+    end
+
+    vim.api.nvim_buf_set_lines(bufferId, 0, -1, false, { "Loading image.." })
     vim.cmd [[ setlocal nofoldenable ]]
     vim.cmd [[ setlocal nonumber ]]
     vim.cmd [[ setlocal colorcolumn=0 ]]
@@ -14,9 +16,8 @@ vim.api.nvim_create_autocmd("BufEnter", {
     vim.cmd [[ setlocal nocursorline ]]
     vim.cmd [[ setlocal nolist ]]
 
-    local filename = vim.api.nvim_buf_get_name(bufferId)
     vim.fn.jobstart(
-      { "timg", "-g80x40", "-E", filename }, {
+      { "timg", "-g80x40", "-E", event.file }, {
         stdout_buffered = true,
         on_stdout = function(_, data)
           if data then
@@ -24,7 +25,10 @@ vim.api.nvim_create_autocmd("BufEnter", {
             baleia.buf_set_lines(bufferId, 0, -1, false, data)
           end
         end,
-        on_exit = function(_, _, _)
+        on_exit = function(_, exitCode, _)
+          if exitCode ~= 0 then
+            vim.api.nvim_buf_set_lines(bufferId, 0, -1, false, { string.format("Error: Exit Code %d", exitCode) })
+          end
           vim.api.nvim_buf_set_option(bufferId, "modifiable", false)
           vim.api.nvim_buf_set_option(bufferId, "buftype", "nowrite")
         end
