93293d9c7e463f3b3266af7b0fe7c07167af1956
neonew <neonew@gmail.com>
2023-04-24 11:30:51 +0200

    nvim: Move files to trash instead of perma-delete them

--- neovim/script.sh
+++ neovim/script.sh
@@ -1,4 +1,4 @@
-sudo pacman -S --needed --noconfirm neovim ripgrep ttf-sourcecodepro-nerd
+sudo pacman -S --needed --noconfirm neovim ripgrep trash-cli ttf-sourcecodepro-nerd
 
 mkdir -p ~/.config/nvim
 git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
--- neovim/user/plugins/neo-tree.lua
+++ neovim/user/plugins/neo-tree.lua
@@ -2,6 +2,15 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
     filesystem = {
+      commands = {
+        -- Override delete to use trash instead of rm
+        -- Taken from here: https://github.com/nvim-neo-tree/neo-tree.nvim/issues/202#issuecomment-1079531421
+        delete = function(state)
+          local path = state.tree:get_node().path
+          vim.fn.system({ "trash", vim.fn.fnameescape(path) })
+          require("neo-tree.sources.manager").refresh(state.name)
+        end
+      },
       filtered_items = {
         hide_dotfiles = false,
         hide_by_name = {
