826db5f552b40efdee4d8da14726086724e83a6e
neonew <neonew@gmail.com>
2023-04-24 11:54:04 +0200

    nvim: CSS/SASS/SCSS support

--- neovim/user/plugins/language-server-protocol.lua
+++ neovim/user/plugins/language-server-protocol.lua
@@ -4,6 +4,7 @@ return {
     automatic_installation = true,
     -- See valid strings here: https://github.com/williamboman/mason-lspconfig.nvim
     ensure_installed = {
+      "cssls",
       "jdtls",
       "jsonls",
       "kotlin_language_server",
