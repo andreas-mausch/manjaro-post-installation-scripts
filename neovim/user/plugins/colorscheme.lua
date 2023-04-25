8a0468d3ca74ad888eb9307f4de79f844e5be36f
neonew <neonew@gmail.com>
2023-04-25 15:05:33 +0200

    nvim: Adjust colorscheme

--- neovim/user/plugins/colorscheme.lua
+++ neovim/user/plugins/colorscheme.lua
@@ -10,8 +10,8 @@ return {
   config = function()
     require("noirbuddy").setup {
       colors = {
-        primary = "#ffb35f",
-        secondary = "#8ba8cd",
+        primary = "#e8d7a5",
+        secondary = "#c4d5e8",
         background = "#151515",
         nb_background = "#151515",
         noir_8 = "#181818",
@@ -22,17 +22,35 @@ return {
       }
     }
 
-    local Group = require('colorbuddy').Group
-    local colors = require('colorbuddy').colors
+    local colors = require("colorbuddy").colors
+    local Group = require("colorbuddy").Group
+    -- local styles = require("colorbuddy").styles
 
     Group.new("Normal", colors.noir_4, colors.nb_background)
     Group.new("NormalNC", colors.primary, colors.noir_9)
     Group.new("CursorLine", nil, colors.black)
+    Group.new("NonText", colors.noir_4, nil)
 
     Group.new("NeoTreeDirectoryIcon", colors.noir_6)
     Group.new("NeoTreeDirectoryName", colors.noir_2)
+    Group.new("NeoTreeFileName", colors.noir_3)
     Group.new("NeoTreeRootName", colors.noir_6)
     Group.new("NeoTreeNormal", colors.noir_4, colors.nb_background)
     Group.new("NeoTreeNormalNC", colors.primary, colors.noir_9)
+    Group.new("NeoTreeGitModified", colors.primary)
+
+    Group.new("GitSignsChange", colors.primary)
+
+    Group.new("IndentBlanklineContextChar", colors.primary)
+    -- Group.new("IndentBlanklineChar", colors.noir_8, nil, styles.nocombine)
+
+    Group.new('TelescopePromptNormal', colors.noir_4, colors.nb_background)
+    Group.new('TelescopeResultsNormal', colors.noir_4, colors.nb_background)
+    Group.new('TelescopePreviewNormal', colors.noir_4, colors.nb_background)
+    Group.new('TelescopeSelection', nil, colors.black)
+    Group.new('TelescopeSelectionCaret', colors.primary)
+    Group.new('TelescopePromptTitle', colors.primary)
+    Group.new('TelescopeResultsTitle', colors.primary)
+    Group.new('TelescopePreviewTitle', colors.primary)
   end
 }
