9368f0e2e12d5c546181913f248ffddab89ba08d
neonew <neonew@gmail.com>
2023-04-25 04:39:05 +0200

    nvim: Adjust highlighting

--- neovim/user/plugins/colorscheme.lua
+++ neovim/user/plugins/colorscheme.lua
@@ -11,7 +11,7 @@ return {
     require("noirbuddy").setup {
       colors = {
         primary = "#ffb35f",
-        secondary = "#95bdf1",
+        secondary = "#8ba8cd",
         diagnostic_error = "#ec0034",
         diagnostic_warning = "#eeff00",
         diagnostic_info = "#d5d5d5"
@@ -21,8 +21,13 @@ return {
     local Group = require('colorbuddy').Group
     local colors = require('colorbuddy').colors
 
+    Group.new("Normal", colors.noir_4, colors.nb_background)
+    Group.new("NormalNC", colors.primary, colors.noir_9)
+
     Group.new("NeoTreeDirectoryIcon", colors.noir_6)
     Group.new("NeoTreeDirectoryName", colors.noir_2)
     Group.new("NeoTreeRootName", colors.noir_6)
+    Group.new("NeoTreeNormal", colors.noir_4, colors.nb_background)
+    Group.new("NeoTreeNormalNC", colors.primary, colors.noir_9)
   end
 }
