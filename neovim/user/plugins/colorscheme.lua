d35bb40396aacf64f2d7f52e56e396e864bd5845
neonew <neonew@gmail.com>
2023-04-25 21:34:50 +0200

    nvim: Clear highlights, disable indent-blankline

--- neovim/user/plugins/colorscheme.lua
+++ neovim/user/plugins/colorscheme.lua
@@ -8,6 +8,8 @@ return {
     }
   },
   config = function()
+    vim.api.nvim_command("highlight clear")
+
     require("noirbuddy").setup {
       colors = {
         primary = "#e8d7a5",
@@ -15,34 +17,34 @@ return {
         background = "#151515",
         nb_background = "#151515",
         noir_8 = "#181818",
-        noir_9 = "#1C1C1C",
-        diagnostic_error = "#ec0034",
+        noir_9 = "#1c1c1c",
+        diagnostic_error = "#e03040",
         diagnostic_warning = "#eeff00",
-        diagnostic_info = "#d5d5d5"
+        diagnostic_info = "#84a5ff",
+        diagnostic_hint = "#84a5ff",
+        diff_add = "#406040",
+        diff_change = "#405060",
+        diff_delete = "#e03040"
       }
     }
 
     local colors = require("colorbuddy").colors
+    local groups = require("colorbuddy").groups
     local Group = require("colorbuddy").Group
-    -- local styles = require("colorbuddy").styles
 
     Group.new("Normal", colors.noir_4, colors.nb_background)
     Group.new("NormalNC", colors.primary, colors.noir_9)
+    Group.link("LineNr", groups.Normal)
     Group.new("CursorLine", nil, colors.black)
-    Group.new("NonText", colors.noir_4, nil)
+    Group.new("CursorLineNr", colors.noir_9, colors.noir_6)
 
     Group.new("NeoTreeDirectoryIcon", colors.noir_6)
     Group.new("NeoTreeDirectoryName", colors.noir_2)
     Group.new("NeoTreeFileName", colors.noir_3)
     Group.new("NeoTreeRootName", colors.noir_6)
-    Group.new("NeoTreeNormal", colors.noir_4, colors.nb_background)
-    Group.new("NeoTreeNormalNC", colors.primary, colors.noir_9)
     Group.new("NeoTreeGitModified", colors.primary)
-
-    Group.new("GitSignsChange", colors.primary)
-
-    Group.new("IndentBlanklineContextChar", colors.primary)
-    -- Group.new("IndentBlanklineChar", colors.noir_8, nil, styles.nocombine)
+    Group.new("NeoTreeGitAdded", colors.secondary)
+    Group.new("NeoTreeGitUntracked", colors.noir_1)
 
     Group.new('TelescopePromptNormal', colors.noir_4, colors.nb_background)
     Group.new('TelescopeResultsNormal', colors.noir_4, colors.nb_background)
