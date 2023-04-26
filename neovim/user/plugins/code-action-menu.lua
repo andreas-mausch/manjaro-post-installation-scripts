221117d0dcf79cb12d0df7705e64c1075f7bafda
neonew <neonew@gmail.com>
2023-04-26 13:31:55 +0200

    nvim: Add quick fix popup menu shortcut

--- neovim/user/plugins/code-action-menu.lua
+++ neovim/user/plugins/code-action-menu.lua
@@ -1,4 +1,7 @@
 return {
   "weilbith/nvim-code-action-menu",
-  cmd = "CodeActionMenu"
+  cmd = "CodeActionMenu",
+  keys = {
+    { "<leader>lq", "<cmd>CodeActionMenu<cr>", desc = "Show quick fix code actions popup menu", mode = { "n", "v" }}
+  }
 }
