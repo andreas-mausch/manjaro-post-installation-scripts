d544667a4d3d0bb017744673975d20ab4bf71e44
neonew <neonew@gmail.com>
2023-04-28 20:31:04 +0200

    less: Retain colors and don't clear screen

--- shell/script.sh
+++ shell/script.sh
@@ -19,3 +19,8 @@ rm -rf ./oh-my-fish
 cp ./fish_prompt.fish ~/.config/fish/functions/
 cp ./fish_variables ~/.config/fish/
 cp ./config.fish ~/.config/fish/
+
+# -r: Show escape symbols, e.g. colors
+# -X: stops the screen from clearing when exiting
+# https://unix.stackexchange.com/questions/19317/can-less-retain-colored-output
+echo "export LESS=-Xr" >> ~/.profile
