bc190ba8967e19e3c0c70e5815e2d782ad58db17
neonew <neonew@gmail.com>
2023-05-11 14:54:01 +0200

    nvim: Show hidden files by default in telescope

--- neovim/user/mappings.lua
+++ neovim/user/mappings.lua
@@ -5,6 +5,7 @@ return {
   n = {
     ["<leader>tt"] = { function() utils.toggle_term_cmd "zenith" end, desc = "ToggleTerm zenith" },
     ["<leader>tu"] = { function() utils.toggle_term_cmd "ncdu" end, desc = "ToggleTerm ncdu" },
+    ["<leader>ff"] = { function() require("telescope.builtin").find_files { hidden = true } end, desc = "Find files" },
     ["?"] = { function() telescope.keymaps({ modes = { "n" } }) end, desc = "Find keymaps" }
   },
   t = {
