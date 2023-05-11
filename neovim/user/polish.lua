a647d0b269da19bf6fbe035fe4cfe42d5d3de5d5
neonew <neonew@gmail.com>
2023-05-11 16:32:53 +0200

    nvim: Do not open neotree on start

--- neovim/user/polish.lua
+++ neovim/user/polish.lua
@@ -1,14 +1,3 @@
 return function()
   require("user/imagePreview")
-
-  vim.api.nvim_create_augroup("neotree", {})
-  vim.api.nvim_create_autocmd({ "UiEnter" }, {
-    desc = "Open Neotree automatically",
-    group = "neotree",
-    callback = function()
-      if vim.fn.argc() == 0 then
-        vim.cmd("Neotree")
-      end
-    end
-  })
 end
