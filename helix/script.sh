fd29b621c003a65d6e67bf4644b3b1ff3d496230
neonew <neonew@gmail.com>
2023-08-28 10:29:42 +0200

    Helix: vue-language-server
    The one based on Volar

--- helix/script.sh
+++ helix/script.sh
@@ -1,5 +1,6 @@
 sudo pacman -S --needed --noconfirm helix
 sudo pacman -S --needed --noconfirm typescript-language-server
+sudo pacman -S --needed --noconfirm vue-language-server
 
 mkdir -p ~/.config/helix/
 cp ./config.toml ~/.config/helix/
