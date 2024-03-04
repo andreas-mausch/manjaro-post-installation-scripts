bf323a6338f016139bf29f67373ccc8641d4c13c
neonew <neonew@gmail.com>
2024-03-04 11:43:47 +0100

    Fix missing sudo on boot animation script

--- boot-animation/script.sh
+++ boot-animation/script.sh
@@ -1,6 +1,6 @@
 # Source of theme: https://github.com/adi1090x/plymouth-themes
 sudo mkdir -p /usr/share/plymouth/themes/motion/
-unzip -q ./motion.zip -d /usr/share/plymouth/themes/motion/
+sudo unzip -q ./motion.zip -d /usr/share/plymouth/themes/motion/
 sudo plymouth-set-default-theme -R motion
 
 # I had to change a line in
