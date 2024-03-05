fa2fd31630509567513694ce3078f89cfe9f739c
neonew <neonew@gmail.com>
2024-03-05 14:35:41 +0100

    ssh: Disable root login

--- ssh/script.sh
+++ ssh/script.sh
@@ -1,4 +1,5 @@
 sudo cp ./20-force_publickey_auth.conf /etc/ssh/sshd_config.d/
+sudo cp ./21-deny_root.conf /etc/ssh/sshd_config.d/
 
 # Put this to ~/.ssh/config in case for TPM 2.0 GPG Key issues
 # Host *
