d32299edd8f76866259bdacf3d28088a3d7d7959
neonew <neonew@gmail.com>
2024-03-08 13:44:34 +0100

    Make slock work with suspend and xflock4

--- slock/script.sh
+++ slock/script.sh
@@ -1,7 +1,16 @@
 sudo pacman -S --needed --noconfirm slock
 sudo pacman -R --noconfirm light-locker xfce4-screensaver
 
-xfconf-query -c xfce4-session -np /general/LockCommand -t string -s ""
+xfconf-query -c xfce4-session -np /general/LockCommand -t string -s "slock"
+
+# Neither the session nor the power manager should invoke slock.
+# It will be called by our own service instead, see below.
+# This is necessary, because otherwise the slock call seems to be blocking,
+# and results in a call to slock, but only after you unlock the system is going to suspend mode.
+# Also, if "LockCommand" is set to an empty string (like I tried before),
+# xflock4 cannot lock the screen anymore (which is something I want and is mapped to Ctrl+Alt+Del).
+xfconf-query -c xfce4-power-manager -np /xfce4-power-manager/lock-screen-suspend-hibernate -t bool -s false
+xfconf-query -c xfce4-session -np /shutdown/LockScreen -t bool -s false
 
 sudo cp ./slock@.service /etc/systemd/system/slock@.service
 sudo systemctl enable slock@${USER}.service
