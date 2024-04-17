2b402a93cb46b5a917f8007e236520835d824e12
neonew <neonew@gmail.com>
2024-04-17 20:34:30 +0200

    Mute microphone on login

--- audacious/script.sh
+++ audacious/script.sh
@@ -8,6 +8,12 @@ mkdir -p ~/.config/audacious/
 cp ./config ~/.config/audacious/
 echo "skin=`realpath ~/.local/share/audacious/Skins/135799-winamp_classic.wsz`" >> ~/.config/audacious/config
 
+# Check usage of pipewire via:
+# pactl info | grep "Server Name"
+
 # There is a "Global Hotkeys plugin", but I'd rather let the
 # PulseAudio panel plugin for XFCE control the media keys,
 # see xfce/xfce4-panel.xml
+
+# This might not be the right place, but mute the microphone on login
+echo "amixer set Capture nocap" >> ~/.profile
