a34c3f0e02151cb5d010d19f1b387c22e4ab4fb2
neonew <neonew@gmail.com>
2024-04-17 20:05:37 +0200

    audacious: Enable global hotkeys

--- audacious/script.sh
+++ audacious/script.sh
@@ -7,3 +7,9 @@ cp ./135799-winamp_classic.wsz ~/.local/share/audacious/Skins/
 mkdir -p ~/.config/audacious/
 cp ./config ~/.config/audacious/
 echo "skin=`realpath ~/.local/share/audacious/Skins/135799-winamp_classic.wsz`" >> ~/.config/audacious/config
+
+# There is a file *~/.config/audacious/plugin-registry*,
+# but it's format is rather unconvenient to copy the whole file over here.
+# Instead, make sure to enable the Options/Plugins/General/Global Hotkeys plugin.
+# I have removed VolumeUp/Down/Mute as this is already covered by PipeWire,
+# but now you can use the system-wide shortcut for XF86AudioPlay!
