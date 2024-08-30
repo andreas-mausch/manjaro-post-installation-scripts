79ba9512071a2ed526dc5268df73f162c73e3353
neonew <neonew@gmail.com>
2024-08-30 12:54:37 +0200

    git-filter-repo

--- git/script.sh
+++ git/script.sh
@@ -1,6 +1,7 @@
 sudo pacman -S --needed --noconfirm tk aspell aspell-en aspell-de
 sudo pacman -S --needed --noconfirm tig lazygit
 sudo pacman -S --needed --noconfirm bat git-delta
+sudo pacman -S --needed --noconfirm git-filter-repo
 
 git config --global alias.lg "log --abbrev-commit --decorate --date=format:'%Y-%m-%d %H:%M:%S' --format=format:'%C(blue)%h%C(reset) %C(dim white)%ad%C(reset) %C(green)%<(8,trunc)%an%C(reset)%C(yellow)%d%C(reset) %C(white)%s%C(reset)'"
 git config --global alias.tidy.branches "! git branch --merged origin/master | grep -v \* | xargs --no-run-if-empty git branch -D && git remote update origin --prune"
