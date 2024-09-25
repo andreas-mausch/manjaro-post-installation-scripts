1f5a0aa883e6c121458cc39d4b4f1344b9ee5223
neonew <neonew@gmail.com>
2024-09-25 18:57:23 +0200

    Chat: Gajim XMPP
    with support for OMEMO and OpenPGP

---
+++ chat/script.sh
@@ -0,0 +1,16 @@
+sudo pacman -S --needed --noconfirm gajim python-gnupg python-gpgme python-axolotl python-qrcode farstream gstreamer gst-libav gst-plugin-gtk gst-plugins-base gst-plugins-ugly gsound gspell
+sudo pacman -S --needed --noconfirm python-gnupg python-gpgme python-axolotl python-qrcode
+sudo pacman -S --needed --noconfirm farstream gstreamer gst-libav gst-plugin-gtk gst-plugins-base gst-plugins-ugly gsound gspell libvoikko nuspell hunspell
+
+# omemo is not a plugin anymore, but merged into Gajim:
+# https://dev.gajim.org/gajim/gajim-plugins/-/commit/719de5a1f4f00bbdd293828973f4bcc5031d1683
+
+mkdir -p ~/.local/share/gajim/plugins/openpgp/
+wget --quiet --directory-prefix /tmp/ https://ftp.gajim.org/plugins/master/openpgp/openpgp_1.6.2.zip
+unzip -q /tmp/openpgp_1.6.2.zip -d ~/.local/share/gajim/plugins/openpgp/
+rm /tmp/openpgp_1.6.2.zip
+
+mkdir -p ~/.local/share/gajim/plugins/pgp/
+wget --quiet --directory-prefix /tmp/ https://ftp.gajim.org/plugins/master/pgp/pgp_1.6.0.zip
+unzip -q /tmp/pgp_1.6.0.zip -d ~/.local/share/gajim/plugins/pgp/
+rm /tmp/pgp_1.6.0.zip
