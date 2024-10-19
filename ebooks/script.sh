879cdb270cd3482daaa6fc90dc475830ff01bed6
neonew <neonew@gmail.com>
2024-10-19 16:23:36 +0200

    zathura: .pdf / .cbr / .cbz / .epub reader

--- ebooks/script.sh
+++ ebooks/script.sh
@@ -1,6 +1,10 @@
 sudo pacman -S --needed --noconfirm calibre
 paru -S --needed --noconfirm yacreader
 
+sudo pacman -S tesseract-data-deu zathura zathura-pdf-mupdf zathura-cb
+mkdir -p ~/.config/zathura/
+cp ./zathurarc ~/.config/zathura/
+
 # ~/.local/share/YACReader/YACReader/YACReader.ini
 # SHOW_INFO=false
 # SHOW_TOOLBARS=false
