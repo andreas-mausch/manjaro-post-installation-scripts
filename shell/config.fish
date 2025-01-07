fb5a00f90e3ed8e77465904c182e2db2abecd508
neonew <neonew@gmail.com>
2025-01-07 20:44:21 +0100

    scana4: Replace alias by function to allow passing arguments to magick

--- shell/config.fish
+++ shell/config.fish
@@ -11,4 +11,5 @@ alias clip="xclip -selection clipboard -rmlastnl"
 alias top="zenith"
 alias lg="lazygit"
 alias crt="cool-retro-term --profile \"Green Scanlines Blinking Cursor\" --fullscreen"
-alias scana4="scanimage --device-name 'hpaio:/usb/OfficeJet_Pro_6970?serial=THXXXXXXX' --progress --format tiff --mode Color --resolution 300dpi -l 0mm -t 0mm -x 210mm -y 297mm | magick convert - -resize '1920x1920>' -quality 75 image.jpg"
+
+function scana4; scanimage --device-name 'hpaio:/usb/OfficeJet_Pro_6970?serial=THXXXXXXX' --progress --format tiff --mode Color --resolution 300dpi -l 0mm -t 0mm -x 210mm -y 297mm | magick convert - $argv -resize '1920x1920>' -quality 75 image.jpg; end
