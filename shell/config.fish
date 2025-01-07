db044136434dd64b19da56b343e2d4919a0e15b9
neonew <neonew@gmail.com>
2025-01-07 20:46:14 +0100

    scana4: Check if file already exists

--- shell/config.fish
+++ shell/config.fish
@@ -12,4 +12,4 @@ alias top="zenith"
 alias lg="lazygit"
 alias crt="cool-retro-term --profile \"Green Scanlines Blinking Cursor\" --fullscreen"
 
-function scana4; scanimage --device-name 'hpaio:/usb/OfficeJet_Pro_6970?serial=THXXXXXXX' --progress --format tiff --mode Color --resolution 300dpi -l 0mm -t 0mm -x 210mm -y 297mm | magick - $argv -resize '1920x1920>' -quality 75 image.jpg; end
+function scana4; if test -e scan.jpg; echo 'File \'scan.jpg\' already exists, aborting.'; return 1; else; scanimage --device-name 'hpaio:/usb/OfficeJet_Pro_6970?serial=THXXXXXXX' --progress --format tiff --mode Color --resolution 300dpi -l 0mm -t 0mm -x 210mm -y 297mm | magick - $argv -resize '1920x1920>' -quality 75 scan.jpg; end; end
