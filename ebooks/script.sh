a0b494f96397aa443ccc004331ad5efa043ec3e0
neonew <neonew@gmail.com>
2025-01-10 03:26:12 +0100

    ocrmypdf

--- ebooks/script.sh
+++ ebooks/script.sh
@@ -1,7 +1,10 @@
 sudo pacman -S --needed --noconfirm calibre
 paru -S --needed --noconfirm yacreader
 
-sudo pacman -S tesseract-data-deu zathura zathura-pdf-mupdf zathura-cb
+sudo pacman -S --needed --noconfirm tesseract-data-deu tesseract-data-eng
+sudo pacman -S --needed --noconfirm zathura zathura-pdf-mupdf zathura-cb
+paru -S --needed --noconfirm ocrmypdf
+
 mkdir -p ~/.config/zathura/
 cp ./zathurarc ~/.config/zathura/
 
