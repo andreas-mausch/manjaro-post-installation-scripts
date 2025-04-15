4caab1a93bcb20e85b239b7c1c5932303b45d328
neonew <neonew@gmail.com>
2025-04-15 20:21:21 +0200

    yt-dlp: Add example to download all playlists from a channel

--- yt-dlp/script.sh
+++ yt-dlp/script.sh
@@ -1,3 +1,7 @@
 sudo pacman -S --needed --noconfirm yt-dlp
 mkdir -p ~/.config/yt-dlp/
 cp ./config ~/.config/yt-dlp/
+
+# https://www.reddit.com/r/youtubedl/comments/rwk1bu/download_all_playlists_by_a_youtube_channel_in/
+# Example call to download all playlists from a channel:
+# yt-dlp -o "%(playlist_title)s/%(upload_date>%Y-%m-%d)s - %(title)+.100U [%(id)s]" --download-archive yt-dlp-archive.txt "https://www.youtube.com/@channelname/playlists" "https://www.youtube.com/@channelname"
