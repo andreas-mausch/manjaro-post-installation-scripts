07cf7601eefa878324df835ff5503be40d23fcf2
neonew <neonew@gmail.com>
2025-05-18 15:37:48 +0200

    qpxtool

--- tools/extra.sh
+++ tools/extra.sh
@@ -1,6 +1,8 @@
 # 123-bin: Lotus 1-2-3 like TUI to edit calc sheets
+# qpxtool: Tool to read the disc quality of optical media (CD-R, DVD-R, BD-R)
 # rawtherapee: Photo editor, like Lightroom
 
 paru -S --needed --noconfirm \
   123-bin \
+  qpxtool-speed47-git \
   rawtherapee
