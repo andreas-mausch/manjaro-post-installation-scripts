c707c2ba9794f3a7644cfc16e2ddd13c36dd4a0c
neonew <neonew@gmail.com>
2025-06-15 20:23:31 +0200

    autorandr: Disable automatic changes, it causes problems with my display

--- autorandr/script.sh
+++ autorandr/script.sh
@@ -1,2 +1,10 @@
 sudo pacman -S --needed --noconfirm autorandr
-sudo systemctl enable --now autorandr.service
+
+# Do not enable the service - we will only call autorandr manually.
+# Otherwise my displays and/or the display driver change rapidly back and forth,
+# until something breaks and the display stays black.
+
+# sudo systemctl enable --now autorandr.service
+
+# Also disable the udev rule
+sudo mv /lib/udev/rules.d/40-monitor-hotplug.rules /lib/udev/rules.d/40-monitor-hotplug.rules.disabled
