e5a3290132a66ae4cbf8e1b78326589a4fe8c231
neonew <neonew@gmail.com>
2026-05-09 23:48:12 +0200

    gpg: set SHA256 preference

--- openpgp/script.sh
+++ openpgp/script.sh
@@ -8,3 +8,6 @@ echo 'max-cache-ttl 14400' >> ~/.gnupg/gpg-agent.conf
 
 echo 'default-cache-ttl-ssh 7200' >> ~/.gnupg/gpg-agent.conf
 echo 'max-cache-ttl-ssh 14400' >> ~/.gnupg/gpg-agent.conf
+
+# This avoids a problem with my TPM not being capable of doing SHA512
+echo 'personal-digest-preferences SHA256' >> ~/.gnupg/gpg.conf
