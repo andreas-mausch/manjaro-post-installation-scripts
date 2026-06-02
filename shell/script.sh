358d81a88d3db54a8a45f365bbecacd8412dd412
neonew <neonew@gmail.com>
2026-06-02 02:02:06 +0200

    shell: Replace unsafe chsh pam hack with sudo chsh

--- shell/script.sh
+++ shell/script.sh
@@ -2,11 +2,7 @@ sudo pacman -S --needed --noconfirm fish
 
 mkdir -p ~/.config/fish/functions/
 
-cp /etc/pam.d/chsh ./chsh.backup
-sudo cp ./chsh /etc/pam.d/
-chsh -s /usr/bin/fish
-sudo cp ./chsh.backup /etc/pam.d/chsh
-rm ./chsh.backup
+sudo chsh -s /usr/bin/fish "$USER"
 
 paru -S --needed --noconfirm autojump-rs
 
