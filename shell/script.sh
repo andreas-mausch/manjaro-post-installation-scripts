1df6eb6eb54c508782d6cdfc908229bb69056496
neonew <neonew@gmail.com>
2026-06-02 02:10:42 +0200

    shell: Guard against duplicate ~/.profile lines

--- shell/script.sh
+++ shell/script.sh
@@ -16,14 +16,18 @@ cp ./fish_prompt.fish ~/.config/fish/functions/
 cp ./fish_variables ~/.config/fish/
 cp ./config.fish ~/.config/fish/
 
+append_once() {
+  grep -qxF "$1" "$2" 2>/dev/null || echo "$1" >> "$2"
+}
+
 # -r: Show escape symbols, e.g. colors
 # -X: stops the screen from clearing when exiting
 # https://unix.stackexchange.com/questions/19317/can-less-retain-colored-output
-echo "export LESS=-Xr" >> ~/.profile
+append_once 'export LESS=-Xr' ~/.profile
 
-echo "export PASSWORD_STORE_SIGNING_KEY=6FB79D6D075A268571DE9E42C2C14B464D7B72E9" >> ~/.profile
+append_once 'export PASSWORD_STORE_SIGNING_KEY=6FB79D6D075A268571DE9E42C2C14B464D7B72E9' ~/.profile
 
-echo "export RCLONE_PASSWORD_COMMAND=\"pass rclone/config\"" >> ~/.profile
+append_once 'export RCLONE_PASSWORD_COMMAND="pass rclone/config"' ~/.profile
 
 # https://github.com/ollama/ollama/pull/4508
-echo "export OLLAMA_NOHISTORY=1" >> ~/.profile
+append_once 'export OLLAMA_NOHISTORY=1' ~/.profile
