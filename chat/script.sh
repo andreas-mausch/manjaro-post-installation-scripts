ad05daf5d72f85c9375abd6cb58849e88ae158a1
neonew <>
2026-07-30 18:36:11 +0200

    Gajim: Update plugin versions and verify downloads

--- chat/script.sh
+++ chat/script.sh
@@ -4,11 +4,13 @@ sudo pacman -S --needed --noconfirm gajim python-gnupg python-gpgme python-axolo
 # https://dev.gajim.org/gajim/gajim-plugins/-/commit/719de5a1f4f00bbdd293828973f4bcc5031d1683
 
 mkdir -p ~/.local/share/gajim/plugins/openpgp/
-wget --quiet --directory-prefix /tmp/ https://ftp.gajim.org/plugins/master/openpgp/openpgp_1.6.2.zip
-unzip -q /tmp/openpgp_1.6.2.zip -d ~/.local/share/gajim/plugins/openpgp/
-rm /tmp/openpgp_1.6.2.zip
+wget --quiet --output-document=/tmp/openpgp.zip https://ftp.gajim.org/plugins/master/openpgp/openpgp_1.8.2.zip
+echo "d8e78a029643f74a48e184619627a26c07b4e39c4c4e4fae0aa1ef635a396b93  /tmp/openpgp.zip" | sha256sum --check --status
+unzip -q /tmp/openpgp.zip -d ~/.local/share/gajim/plugins/openpgp/
+rm /tmp/openpgp.zip
 
 mkdir -p ~/.local/share/gajim/plugins/pgp/
-wget --quiet --directory-prefix /tmp/ https://ftp.gajim.org/plugins/master/pgp/pgp_1.6.0.zip
-unzip -q /tmp/pgp_1.6.0.zip -d ~/.local/share/gajim/plugins/pgp/
-rm /tmp/pgp_1.6.0.zip
+wget --quiet --output-document=/tmp/pgp.zip https://ftp.gajim.org/plugins/master/pgp/pgp_1.8.2.zip
+echo "f231364533f6893b853139e52745b003bda64aa7c9a09d07ba6aaccebb40527e  /tmp/pgp.zip" | sha256sum --check --status
+unzip -q /tmp/pgp.zip -d ~/.local/share/gajim/plugins/pgp/
+rm /tmp/pgp.zip
